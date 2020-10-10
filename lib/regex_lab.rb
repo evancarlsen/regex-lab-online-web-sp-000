37ad98ec4ce41c7fbfbdf34ee37e4c4119cca573
Evan James Carlsen <evan@evancarlsen.com>
2020-10-10 22:16:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,7 +24,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- if(phone[0] == "0")
+ if(phone[0] == "\(")
    return phone
  end
  return "you"
