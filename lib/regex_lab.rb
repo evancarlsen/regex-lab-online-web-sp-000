62e08818996b4125e0c652265eea13a306b4e543
Evan James Carlsen <evan@evancarlsen.com>
2020-10-10 22:21:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,7 +24,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- return phone.tr('^0-9', '') 
+ x =  phone.tr('^0-9', '') 
+ y = x.grep(/^.{10}$/)
 end
 
 puts valid_phone_number?("\(999\)999 9999")
