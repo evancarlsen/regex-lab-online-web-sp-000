b4a184108ec090afee12a01f5744800d6962a757
Evan James Carlsen <evan@evancarlsen.com>
2020-10-10 22:20:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,7 +24,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- return phone..scan(/\d+/)
+ return phone.scan(/\d+/)
 end
 
 puts valid_phone_number?("\(999\)999 9999")
