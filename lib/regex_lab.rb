080c34ece14bf7e745518b4205ed56403cb9eab7
Evan James Carlsen <evan@evancarlsen.com>
2020-10-10 22:13:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,6 +24,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
- x = phone.match(/^[2-9]\d{2}-\d{3}-\d{4}$/)
- return (x !=nil)
+ if(phone[0] == "(")
+   return phone
 end
